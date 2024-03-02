bccf5e9d0b58c6b98121d9266326ec37ac1737d6
abenable <ableabenaitwe@gmail.com>
2024-03-02 15:31:40 +0300

    Update Vite configuration and add deployment workflow

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: "/hub-256/",
-  base: "./",
+  base: "/hub-256/",
   plugins: [react()],
 });
