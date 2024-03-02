3ccbcb9d88882ba1f500f16fcda03e10fbb91cdc
abenable <ableabenaitwe@gmail.com>
2024-03-02 14:39:57 +0300

    Update base URL in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: "/hub-256_client/",
+  base: "/hub-256/",
   plugins: [react()],
 });
