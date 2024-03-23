dc0a6fa60f8898a9808829c750c47d894ad3a533
abenable <ableabenaitwe@gmail.com>
2024-03-23 17:05:43 +0300

    Add Tailwind CSS and Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Routes, Route } from "react-router-dom";
-import Layout from "./pages/layout/layout";
 import About from "./pages/about/about";
 import TestPage from "./pages/testpage/test";
 import Index from "./pages/index";
 import Login from "./pages/login/login";
+import { NavbarMenu } from "./components/navbar/navbar";
 
 const App = () => {
   return (
     <div className="App">
-      <Layout />
+      <NavbarMenu />
       <Routes>
         <Route path="" element={<Index />} />
         <Route path="/about" element={<About />} />
