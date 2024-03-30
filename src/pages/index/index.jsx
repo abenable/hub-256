89a2c02cec68eee05d4e010a52a8ed30871ca2fe
Able Abenaitwe <ableabenaitwe@gmail.com>
2024-03-30 14:21:50 +0300

    Update index.jsx

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -48,7 +48,7 @@ export default function IndexPage() {
 
   return (
     <>
-      <HorizontalCard post={recommendedPost} />
+      <HorizontalCard post={recommendedPost[0]} />
       <BrowseCard />
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
         {posts.map((post) => (
