8a85c823411aa5da93dfc4cd4de3e61208c9570a
abenable <ableabenaitwe@gmail.com>
2024-03-30 20:56:56 +0300

    Refactor newsletter section styling

--- src/components/newsletter/newsletter.jsx
+++ src/components/newsletter/newsletter.jsx
@@ -2,8 +2,8 @@ import { Button, Input, Typography } from "@material-tailwind/react";
 
 export function NewsLetter() {
   return (
-    <section className="mt-10 p-10">
-      <div className="container mx-auto flex !w-full max-w-6xl flex-col !items-center justify-center rounded-2xl bg-gray-800 px-6 py-16">
+    <section className="mt-8 p-6">
+      <div className="container mx-auto flex !w-full max-w-6xl flex-col !items-center justify-center rounded-2xl bg-gray-800 px-6 py-10">
         <Typography
           className="text-center text-2xl font-bold md:text-3xl"
           color="white"
