8fdfc7124f32f7d61ff094b1f1595ca116ed46a8
abenable <ableabenaitwe@gmail.com>
2024-04-04 22:19:31 +0300

    Refactor LatestPostsCard component

--- src/components/cards/latest.jsx
+++ src/components/cards/latest.jsx
@@ -1,7 +1,6 @@
 import {
   Card,
   CardHeader,
-  CardFooter,
   CardBody,
   Typography,
   Button,
@@ -9,26 +8,41 @@ import {
 
 export default function LatestPostsCard({ post }) {
   return (
-    <Card className="mt-10 mx-6 sm:w-1/2 md:w-1/2 lg:w-1/2">
-      <CardHeader color="blue-gray" className="relative h-30">
-        <img src={post.urlToImg} alt="card-image" />
+    <Card className="`bg-white w-1/2 shadow-md rounded-md p-2 flex flex-row md:flex-row sm:flex-row items-start md:items-center justify-between mx-2 my-6 md:flex">
+      <CardHeader
+        shadow={true}
+        className="h-full w-full m-0 shrink-0 rounded-md md:w-2/5 rounded-t-md md:rounded-t-none md:rounded-l-md"
+      >
+        <img
+          className="h-32 w-full rounded-lg object-cover object-center shadow-xl shadow-blue-gray-900/50"
+          src={post.urlToImg}
+          alt="nature image"
+        />
       </CardHeader>
       <CardBody>
-        <Typography variant="h5" color="blue-gray" className="mb-2">
+        <Typography
+          variant="h6"
+          color="purple"
+          className="mb-4 inline-flex text-purple-700 bg-purple-50 bg-opacity-50 font-medium text-base px-3 rounded-full capitalize"
+        >
+          {post.category}
+        </Typography>
+        <Typography
+          variant="h5"
+          color="blue-gray"
+          className="mb-2 sm:text-medium "
+        >
           {post.title}
         </Typography>
-        <Typography>{post.description}</Typography>
-      </CardBody>
-      <CardFooter className="pt-0">
         <a href={post.learnMoreUrl} className="inline-block">
-          <Button size="sm" variant="text" className="flex items-center gap-2">
+          <Button variant="text" className="flex items-center gap-2">
             Learn More
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
-              strokeWidth={2}
               stroke="currentColor"
+              strokeWidth={2}
               className="h-4 w-4"
             >
               <path
@@ -39,7 +53,7 @@ export default function LatestPostsCard({ post }) {
             </svg>
           </Button>
         </a>
-      </CardFooter>
+      </CardBody>
     </Card>
   );
 }
