23ab44e82f439648e840efccfd9dd0d0638b12c5
abenable <ableabenaitwe@gmail.com>
2024-04-05 18:33:56 +0300

    Add "Latest blogs" heading to index page

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -2,7 +2,8 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import BrowseCard from "../../components/cards/browseCard";
 import { HorizontalCard } from "../../components/cards/horizontalCard";
-import { Spinner } from "@material-tailwind/react";
+import { Spinner, Typography } from "@material-tailwind/react";
+
 import LatestPostsCard from "../../components/cards/latest";
 
 export default function IndexPage() {
@@ -46,15 +47,20 @@ export default function IndexPage() {
           <Spinner className="h-16 w-16 text-gray-900/50" />
         </div>
       )}
-      {latestPosts ? (
-        latestPosts.map((post) => (
-          <LatestPostsCard key={post._id} post={post} />
-        ))
-      ) : (
-        <div className="justify-center items-center flex">
-          <Spinner className="h-16 w-16 text-gray-900/50" />
-        </div>
-      )}
+      <Typography variant="h3" className="items-center justify-center flex">
+        Latest blogs
+      </Typography>
+      <div className="flex flex-row gap-3">
+        {latestPosts ? (
+          latestPosts.map((post) => (
+            <LatestPostsCard key={post._id} post={post} />
+          ))
+        ) : (
+          <div className="justify-center items-center flex">
+            <Spinner className="h-16 w-16 text-gray-900/50" />
+          </div>
+        )}
+      </div>
       <BrowseCard />
     </>
   );
