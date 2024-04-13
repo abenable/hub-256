836e4819fb91a86c978b7aa7bf3c83d0b9f0d731
abenable <ableabenaitwe@gmail.com>
2024-04-13 17:18:51 +0300

    Update tailwind.config.js to add Deep Purple color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,20 @@ export default withMT({
     "node_modules/flowbite-react/lib/esm/**/*.js",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "Deep-Purple-50": "#ede7f6",
+        "Deep-Purple-100": "#d1c4e9",
+        "Deep-Purple-200": "#b39ddb",
+        "Deep-Purple-300": "#9575cd",
+        "Deep-Purple-400": "#7e57c2",
+        "Deep-Purple-500": "#673ab7",
+        "Deep-Purple-600": "#5e35b1",
+        "Deep-Purple-700": "#512da8",
+        "Deep-Purple-800": "#4527a0",
+        "Deep-Purple-900": "#311b92",
+      },
+    },
   },
   plugins: [flowbitePlugin],
 });
