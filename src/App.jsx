65be8e98be0c7033b8ef3623ec8f7fb4a1c015d3
abenable <ableabenaitwe@gmail.com>
2024-04-13 17:19:08 +0300

    Update App.jsx to add mx-4 class to the App div

--- src/App.jsx
+++ src/App.jsx
@@ -37,7 +37,7 @@ function App() {
     return () => window.removeEventListener("scroll", toggleVisibility);
   }, []);
   return (
-    <div className="App">
+    <div className="App mx-4">
       <Navbar />
 
       <Routes>
