a0b1de69eabc0f98771985f649636d9ba26dd2bd
abenable <ableabenaitwe@gmail.com>
2024-04-13 17:20:15 +0300

    Update Navbar component to add logo and adjust button styles

--- src/components/navbar/navbar2.0.jsx
+++ src/components/navbar/navbar2.0.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import image from "./logo.jpg";
 import {
   Collapse,
   Typography,
@@ -158,23 +159,16 @@ const Navbar = () => {
       <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <a
-            href="https://flowbite.com/"
+            href="#"
             className="flex items-center space-x-3 rtl:space-x-reverse"
           >
-            <img
-              src="https://flowbite.com/docs/images/logo.svg"
-              className="h-8"
-              alt="Flowbite Logo"
-            />
-            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-              Hub256
-            </span>
+            <img src={image} className="h-12" alt="Flowbite Logo" />
           </a>
           <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
             <button
               type="button"
               onClick={handleOpen}
-              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              className="text-white bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
             >
               Get started
             </button>
